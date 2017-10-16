f57bc816317310c8a1a59db4da92323265d8ce2f
Spiros Raptis <spirosrap@gmail.com>
2017-10-17 02:33:03 +0300

    draw the main features

--- minip1-cv-mimic/mimic.js
+++ minip1-cv-mimic/mimic.js
@@ -148,14 +148,17 @@ function drawFeaturePoints(canvas, img, face) {
   // TODO: Set the stroke and/or fill style you want for each feature point marker
   // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D#Fill_and_stroke_styles
   // <your code here>
-  
+  ctx.strokeStyle = 'white'
   // Loop over each feature point in the face
   for (var id in face.featurePoints) {
     var featurePoint = face.featurePoints[id];
-
     // TODO: Draw feature point, e.g. as a circle using ctx.arc()
     // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
     // <your code here>
+    ctx.beginPath();
+	ctx.arc(featurePoint.x, featurePoint.y, 2,0 , 2 * Math.PI);
+	ctx.stroke();
+
   }
 }
 
