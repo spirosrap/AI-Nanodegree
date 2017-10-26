e9ee76838362f463dc9459de6e56ee256eccd92b
Spiros Raptis <spirosrap@gmail.com>
2017-10-26 03:29:41 +0300

    asd

--- minip3-alexa-vui/src/facts.js
+++ minip3-alexa-vui/src/facts.js
@@ -7,6 +7,10 @@ module.exports = {
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "In January 2015, Stephen Hawking, Elon Musk, and dozens of Artificial Intelligence experts signed an open letter on AI, calling for research on the societal impacts and urging researchers not to create something which cannot be controlled",
+        "Billionaire Dmitry Itskov created a non-profit organization called the 2045 Initiative, where they want to make people immortal by adding human intelligence into artificial bodies by the year 2045",
+        "The 1983 Automonopoli was a computer version of Monopoly marketed as the first to have an Artificial Intelligence strong enough to compete against human players.",
+        "As far back as 2006 NASA was using Artificial Intelligence to perform evolutionary processes that created an optimally designed space antenna after millions of iterations in only 10 hours.",
+        "In 1966 a group of M.I.T. students used a computer in the Artificial Intelligence lab to determine the fastest route through the entire New York City subway system, which they took during their Spring Break."
     ]
 }
